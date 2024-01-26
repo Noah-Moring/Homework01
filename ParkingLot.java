03e55d508c6dda7fe81efb0f602351c00e376110
Noah-Moring <148168395+Noah-Moring@users.noreply.github.com>
2024-01-26 11:51:23 -0500

    Initial

---
+++ ParkingLot.java
@@ -0,0 +1,20 @@
+public class ParkingLot {
+    public ParkingSpot[] spots;
+
+    public ParkingLot(){
+        this.spots = new ParkingSpot[10];
+        for (int i = 0; i < 10; i++){
+            if (i <= 1){
+                this.spots[i] = new ParkingSpot(true);
+            }
+            else{
+                this.spots[i] = new ParkingSpot(false);
+            }
+        }
+    }
+
+    public String toString(){
+
+        return "";
+    }
+}
