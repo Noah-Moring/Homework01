07386a089e2c696f7685c8401e737b29527cbdc9
Noah-Moring <148168395+Noah-Moring@users.noreply.github.com>
2024-01-31 11:23:46 -0500

    ParkingLotV1

--- ParkingLot.java
+++ ParkingLot.java
@@ -13,8 +13,61 @@ public class ParkingLot {
         }
     }
 
-    public String toString(){
+    public int ParkCar(Car x){
 
-        return "";
+        if(x.handicap){
+            for(int i = 0; i <=1; i++){
+                if(spots[i].parkedCar == null){
+                    spots[i].parkedCar = x;
+                    return i;
+                }
+
+            }
+        }
+        else{
+            for(int i = 2; i <= 10; i++){
+                if(spots[i].parkedCar == null){
+                    spots[i].parkedCar = x;
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+
+    public Car removeCar(int y){
+        if(spots[y].parkedCar != null){
+            Car z = spots[y].parkedCar;
+            spots[y].parkedCar = null;
+            return z;
+            
+        }
+        else{
+            // Car z = null;
+            // return z;
+            return spots[y].parkedCar;
+        }
+        
+    }
+
+    public String toString(){
+        // for(int i = 0; i < 10, i++){
+        //     if(spots[i].parkedCar)
+        // }
+        int x = 0;
+        int y = 0;
+        for(int i = 0; i < 10; i++){
+            if(spots[i].parkedCar == null){
+                if(spots[i].handicap == true){
+                    x = x + 1;
+                }
+                else{
+                    y = y + 1;
+                }
+
+            }
+        }
+        String spotString = x +" "+ y;
+        return spotString;
     }
 }
