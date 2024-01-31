c5c5dcd7eea9d699b33444b3f27877365ce3c578
Noah-Moring <148168395+Noah-Moring@users.noreply.github.com>
2024-01-31 12:35:57 -0500

    ParkingTesterWorkingV1

--- ParkingTester.java
+++ ParkingTester.java
@@ -1,6 +1,20 @@
 public class ParkingTester {
-    public static void main(){
+    public static void main(String[] args){
+        ParkingLot mainParkingLot = new ParkingLot();
+
+        System.out.println(mainParkingLot);
+
+        Car infinittiA = new Car("A", "Infinitti", true );
+        mainParkingLot.ParkCar(infinittiA);
+        System.out.println(mainParkingLot);
+
+        Car cadillacB = new Car("B", "Cadillac", false);
+        mainParkingLot.ParkCar(cadillacB);
+        System.out.println(mainParkingLot);
+
+        mainParkingLot.removeCar(0);
+        System.out.println(mainParkingLot);
 
     }
-    }
+    
 }
